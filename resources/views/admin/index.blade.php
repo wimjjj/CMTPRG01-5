c3d770011aabe63d3a42ca8534a0959fd05bcc14
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:27:28 +0100

    new admin-index-panel style

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-            	<div class="panel-heading">Admin</div>
-            	<div class="panel-body">
-            		<a href="{{ Route('admin.users') }}" class="btn btn-default">users</a>
+    <div class="container">
+        <div class="row">
+            <h1>Admin</h1>
+            <a href="{{ Route('admin.users') }}" class="btn btn-default">users</a>
 
-            		<a href="{{ Route('admin.parties') }}" class="btn btn-default">parties</a>
-            	</div>
-            </div>
+            <a href="{{ Route('admin.parties') }}" class="btn btn-default">parties</a>
         </div>
     </div>
-</div>
 @endsection
