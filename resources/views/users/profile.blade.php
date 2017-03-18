6aaacc4cb429a41ef3d9e0c902024dee32e5a39c
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:48:08 +0100

    new profile style

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Profile</div>
-                <div class="panel-body">
-                	@if($user->id == Auth::id())
-			        	<a href="{{ Route('profile.edit') }}" class="btn btn-default pull-right">
-							edit
-						</a>
-					@endif
-					
-					<p><b>name:</b> {{{ $user->name }}}</p>
-					<p><b>email:</b> {{{ $user->email }}}</p>
-				</div>
-			</div>
+    <div class="container">
+        <div class="row">
+            <h1>Profile</h1>
+            @if($user->id == Auth::id())
+                <a href="{{ Route('profile.edit') }}" class="btn btn-default pull-right">
+                    edit
+                </a>
+            @endif
+
+            <p><b>name:</b> {{ $user->name }}</p>
+            <p><b>email:</b> {{ $user->email }}</p>
         </div>
     </div>
-</div>
 @endsection
