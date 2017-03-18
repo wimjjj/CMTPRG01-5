b2a862bca1334c8d8e6a6d6a6154828905122d8f
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:13:01 +0100

    new task-list style

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,45 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Tasks for: {{{ $party->name }}}
-                    <p class="pull-right">
-                        <a href="{{ Route('party.show', ['id' => $party->id]) }}">
-                            party
-                        </a>
-                        @if($party->owner->id == Auth::id())
-                             |
-                            <a href="{{ Route('party.tasks.new', ['id' => $party->id]) }}">
-                                add task
-                            </a>
-                        @endif
-                    </p>                  
-                </div>
-                <div class="panel-body">
-                    @foreach($tasks as $task)
-                        <div>
-                            <a class="pull-right" href="{{ Route('task.show', ['id' => $task->id]) }}">details</a>
-                            <p> {{{ $task->description }}} </p>
-                            taken by: 
-                            @if($task->user)
-                                {{{ $task->user->name }}}
-                            @else
-                                <a class="pull-right" href="{{ Route('task.claim', ['id' => $task->id])}}">
-                                    claim
-                                </a>
-                                none one yet
-                            @endif
-                            <hr>
-                        </div>
-                    @endforeach
-                    {{ $tasks->links() }}
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="pull-right">
+                <a href="{{ Route('party.show', ['id' => $party->id]) }}" class="btn btn-link">
+                    PARTY
+                </a>
+                @if($party->owner->id == Auth::id())
+                    <a href="{{ Route('party.tasks.new', ['id' => $party->id]) }}" class="btn btn-link">
+                        NEW
+                    </a>
+                @endif
             </div>
+            <h1>Tasks for: {{ $party->name }}</h1>
+
+            @foreach($tasks as $task)
+                <hr>
+                <a href="{{ Route('task.show', ['id' => $task->id]) }}">
+                    <div class="task-item">
+                        <p> {{ $task->description }} </p>
+                        taken by: {{ $task->user ? $task->user->name : 'none one yet' }}
+                    </div>
+                </a>
+            @endforeach
+
+            {{ $tasks->links() }}
         </div>
     </div>
-</div>
 @endsection
