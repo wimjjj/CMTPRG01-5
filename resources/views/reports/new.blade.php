65be557fe90c2d4ca68d2ca9e70042a76bb52caf
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:43:11 +0100

    new report-invitation style

--- resources/views/reports/new.blade.php
+++ resources/views/reports/new.blade.php
@@ -1,42 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Report {{{ $party->name }}}</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ Route('report.store') }}">
-                        <input type="hidden" name="party_id" value="{{ $party->id }}">
+    <div class="container">
+        <div class="row">
+            <h1>Report {{ $party->name }}</h1>
 
-                        {{ csrf_field() }}
-                        
-                        <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
-                            <label for="message" class="col-md-4 control-label">message</label>
-          
-                            <div class="col-md-6">
-                                <input id="message" type="text" class="form-control" name="message" required" value="{{{ old('message') }}}">
+            <form class="form-horizontal col-sm-6" role="form" method="POST" action="{{ Route('report.store') }}">
+                <input type="hidden" name="party_id" value="{{ $party->id }}">
 
-                                @if ($errors->has('message'))
-                                    <span class="help-block">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
+                    <label for="message" class="control-label">message</label>
+
+                    <input id="message" type="text" class="form-control" name="message" required
+                           value="{{ old('message') }}">
+
+                    @if ($errors->has('message'))
+                        <span class="help-block">
                                         <strong>{{ $errors->first('message') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Report
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    @endif
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                        Report
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
