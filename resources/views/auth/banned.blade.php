0145909fc7e2d7cb06bae99521d58142cf0ccfb5
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:34:42 +0100

    new login & banned style

--- resources/views/auth/banned.blade.php
+++ resources/views/auth/banned.blade.php
@@ -3,14 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Banned</div>
-                <div class="panel-body">
-                	You are banned
-                </div>
-            </div>
-        </div>
+        <h1>You Are Banned</h1>
     </div>
 </div>
 @endsection
