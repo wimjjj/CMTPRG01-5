01df2f71457d4d9af8800b2cfcc4461dfb5e9c34
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:38:30 +0100

    new update-password style

--- resources/views/auth/passwords/chance.blade.php
+++ resources/views/auth/passwords/chance.blade.php
@@ -1,63 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-			<form class="form-horizontal" role="form" method="POST" action="{{ url('/password') }}">
+    <div class="container">
+        <div class="row">
+            <h1>Update Password</h1>
+            <form class="form-horizontal col-sm-6" role="form" method="POST" action="{{ url('/password') }}">
                 {{ csrf_field() }}
-				
-				<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">Old Password</label>
-  
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control" name="password" required>
 
-                        @if ($errors->has('password'))
-                            <span class="help-block">
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="control-label">Old Password</label>
+
+                    <input id="password" type="password" class="form-control" name="password" required>
+
+                    @if ($errors->has('password'))
+                        <span class="help-block">
                                 <strong>{{ $errors->first('password') }}</strong>
                             </span>
-                        @endif
-                    </div>
+                    @endif
                 </div>
 
-				<div class="form-group{{ $errors->has('new_password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">New Password</label>
+                <div class="form-group{{ $errors->has('new_password') ? ' has-error' : '' }}">
+                    <label for="password" class="control-label">New Password</label>
 
-                    <div class="col-md-6">
-                        <input id="new_password" type="password" class="form-control" name="new_password" required>
+                    <input id="new_password" type="password" class="form-control" name="new_password" required>
 
-                        @if ($errors->has('new_password'))
-                            <span class="help-block">
+                    @if ($errors->has('new_password'))
+                        <span class="help-block">
                                 <strong>{{ $errors->first('new_password') }}</strong>
                       	    </span>
-                        @endif
-                    </div>
-               	</div>
+                    @endif
+                </div>
 
                 <div class="form-group{{ $errors->has('new_password_confirmation') ? ' has-error' : '' }}">
-                    <label for="password-confirm" class="col-md-4 control-label">Confirm New Password</label>
-  
-                    <div class="col-md-6">
-                        <input id="new_password_confirmation" type="password" class="form-control" name="new_password_confirmation" required>
+                    <label for="password-confirm" class="control-label">Confirm New Password</label>
 
-                        @if ($errors->has('new_password_confirmation'))
-                            <span class="help-block">
+                    <input id="new_password_confirmation" type="password" class="form-control"
+                           name="new_password_confirmation" required>
+
+                    @if ($errors->has('new_password_confirmation'))
+                        <span class="help-block">
                                 <strong>{{ $errors->first('new_password_confirmation') }}</strong>
                             </span>
-                        @endif
-                    </div>
+                    @endif
                 </div>
 
                 <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">
-                            Chance Password
-                        </button>
-                    </div>
+                    <button type="submit" class="btn btn-primary">
+                        Chance Password
+                    </button>
                 </div>
             </form>
         </div>
     </div>
-</div>
 @endsection
