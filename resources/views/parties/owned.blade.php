b921713d156ad5e6447a8701851d8820440be41a
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 16:47:41 +0100

    new all-my-parties-page style

--- resources/views/parties/owned.blade.php
+++ resources/views/parties/owned.blade.php
@@ -1,26 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Organised by me</div>
-                <div class="panel-body">
-                	@foreach($parties as $party)
-                		<div>
-                			<a href="{{ Route('party.show', ['id' => $party->id]) }}">
-                				<h3>{{{ $party->name}}}</h3>
-                			</a>
-                			<p>{{{ $party->description}}}</p>
-                			<hr>
-                		</div>
-                	@endforeach
-
-                	{{ $parties->links() }}
+    <div class="container">
+        <div class="row">
+            <h1>Organised by me</h1>
+            @foreach($parties as $party)
+                <hr>
+                <div class="party-item">
+                    <a href="{{ Route('party.show', ['id' => $party->id]) }}">
+                        <h3>{{ $party->name }}</h3>
+                    </a>
+                    <p>{{ $party->description }}</p>
                 </div>
-            </div>
+            @endforeach
+
+            {{ $parties->links() }}
         </div>
     </div>
-</div
 @endsection
