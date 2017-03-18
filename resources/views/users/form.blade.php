d7f71cce500a33651d8b06c9b6a2a4e45fd446a9
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:47:04 +0100

    new update-profile style

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,40 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-        <div class="col-md-8 col-md-offset-2">
-        	<div class="panel panel-default">
-                <div class="panel-heading">Update Profile</div>
-                <div class="panel-body">
-		    		<form class="form-horizontal" role="form" method="POST" action="{{ Route('profile.me') }}">
-		                {{ csrf_field() }}
-						
-						<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-		                    <label for="name" class="col-md-4 control-label">name</label>
-		  
-		                    <div class="col-md-6">
-		                        <input id="email" type="text" class="form-control" name="name" required value="{{{ old('name') != null ? old('name') : $user->name  }}}">
+    <div class="container">
+        <div class="row">
+            <h1>Update Profile</h1>
+            <form class="form-horizontal col-sm-6" role="form" method="POST" action="{{ Route('profile.me') }}">
+                {{ csrf_field() }}
 
-		                        @if ($errors->has('name'))
-		                            <span class="help-block">
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="name" class="control-label">name</label>
+
+                    <input id="email" type="text" class="form-control" name="name" required
+                           value="{{ old('name') != null ? old('name') : $user->name  }}">
+
+                    @if ($errors->has('name'))
+                        <span class="help-block">
 		                                <strong>{{ $errors->first('name') }}</strong>
 		                            </span>
-		                        @endif
-		                    </div>
-		                </div>
+                    @endif
+                </div>
 
-		                <div class="form-group">
-		                    <div class="col-md-6 col-md-offset-4">
-		                        <button type="submit" class="btn btn-primary">
-		                            Save
-		                        </button>
-		                    </div>
-		                </div>
-		            </form>
-		        </div>
-		    </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                        Save
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
