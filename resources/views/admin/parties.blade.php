f9f428a250428d5165c65f2968b51d524d0ebe01
wimjjj <wim.jongeneel1@gmail.com>
2017-03-18 17:25:44 +0100

    new admin-parties-panel style

--- resources/views/admin/parties.blade.php
+++ resources/views/admin/parties.blade.php
@@ -1,50 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-            	<div class="panel-heading">Parties</div>
-            	<div class="panel-body">
-                    <table class="table">
-                    <thead>
-                        <th>name</th>
-                        <th>owner</th>
-                        <th>reports</th>
-                        <th>delete</th>
-                    </thead>
-                    <tbody>
-            		    @foreach($parties as $party)
-                            <tr>
-                                <td>
-                                    {{{ $party->name }}}
-                                </td>
-                                <td>
-                                    <a href="{{ route('profile', ['id' => $party->owner->id]) }}"> 
-                                        {{{ $party->owner->name }}}
-                                    </a>
-                                </td>
-                                <td>    
-                                    <a href="{{ route('admin.parties.reports', ['partyid' => $party->id]) }}"> 
-                                        {{ $party->reports_count }}
-                                    </a>
-                                </td>
-                                <td>
-                                    <form method="post" action="{{ route('admin.parties.delete') }}">
-                                        <input type="hidden" name="party" value="{{ $party->id }}">
-                                        {{ csrf_field() }}
-                                        <input type="submit" value="delete" class="btn btn-danger">
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                    </table>
-                    {{ $parties->links() }}
-            	</div>
-            </div>
-        </div>
+    <div class="container">
+        <div class="row">
+            <h1>Parties</h1>
+            <table class="table">
+                <thead>
+                <th>name</th>
+                <th>owner</th>
+                <th>reports</th>
+                <th>delete</th>
+                </thead>
+                <tbody>
+                @foreach($parties as $party)
+                    <tr>
+                        <td>
+                            {{ $party->name }}
+                        </td>
+                        <td>
+                            <a href="{{ route('profile', ['id' => $party->owner->id]) }}">
+                                {{ $party->owner->name }}
+                            </a>
+                        </td>
+                        <td>
+                            <a href="{{ route('admin.parties.reports', ['partyid' => $party->id]) }}">
+                                {{ $party->reports_count }}
+                            </a>
+                        </td>
+                        <td>
+                            <form method="post" action="{{ route('admin.parties.delete') }}">
+                                <input type="hidden" name="party" value="{{ $party->id }}">
+                                {{ csrf_field() }}
+                                <input type="submit" value="delete" class="btn btn-danger">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        {{ $parties->links() }}
     </div>
-</div>
 @endsection
